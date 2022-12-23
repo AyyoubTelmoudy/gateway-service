8c43465073649aaf3799be20f02642cfd6932d5c
AyyoubTelmoudy <91156099+AyyoubTelmoudy@users.noreply.github.com>
2022-12-23 01:44:29 +0100

    Update GatewayServiceApplication.java
    ljslfjlsdkf

--- src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -5,23 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import java.util.Arrays;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
+	@Bean
+	DiscoveryClientRouteDefinitionLocator definitionLocator(ReactiveDiscoveryClient client, DiscoveryLocatorProperties properties){
+		return new DiscoveryClientRouteDefinitionLocator(client,properties);
 
+	}
 
 }
